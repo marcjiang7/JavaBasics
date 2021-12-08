c1dd26cce4311b222d1033acb54367c44bbb0949
unknown <mjiang05@gmail.com>
2021-12-08 17:38:09 -0500

    Powers of two

---
+++ GitBasics.java
@@ -0,0 +1,13 @@
+public class GitBasics {
+    public static void main(String[] args){
+      problem1();  
+    }
+    
+    public static void problem1(){
+        int x = 1;
+        while(x<694){
+            System.out.println(x);
+            x*=2;
+        }
+    } 
+}
